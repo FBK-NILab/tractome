0acf141942281f2c0f8d8cbfdce2af59ce38fd54
maharshigor <gor.maharshi@gmail.com>
2025-06-03 23:11:43 -0400

    NF: T1 support added!

--- tractome/main.py
+++ tractome/main.py
@@ -1,23 +1,26 @@
 import os
 
 import numpy as np
+from dipy.io.image import load_nifti
 from dipy.io.streamline import load_tractogram
 from fury import actor, window
 from fury.colormap import distinguishable_colormap
 from fury.geometry import buffer_to_geometry, create_mesh
 from fury.lib import QtWidgets
 from fury.material import _create_mesh_material
+from fury.utils import get_slices, show_slices
 from plyfile import PlyData
 
 app = QtWidgets.QApplication([])
 
 
 class Tractome(QtWidgets.QWidget):
-    def __init__(self, *, streamlines=None, mesh=None):
+    def __init__(self, *, streamlines=None, mesh=None, t1=None):
         super().__init__(None)
         self._initUI()
         self._streamlines = streamlines
         self._mesh = mesh
+        self._t1 = t1
         self._color_generator = distinguishable_colormap()
         self._init_actors()
         window.update_camera(self.show_manager.screens[0].camera, None, self.scene)
@@ -29,6 +32,18 @@ class Tractome(QtWidgets.QWidget):
         else:
             self.scene.remove(self._mesh_actor)
             self._mesh_button.setText("Show Mesh")
+        self.show_manager.render()
+
+    def _on_key_press(self, event):
+        position = get_slices(self._t1_actor)
+        if event.key == "ArrowUp":
+            position += 1
+        elif event.key == "ArrowDown":
+            position -= 1
+
+        position = np.maximum(np.zeros((3,)), position)
+        position = np.minimum(np.asarray(self._t1.shape), position)
+        show_slices(self._t1_actor, position)
         self.show_manager.render()
 
     def _on_streamlines_button_click(self):
@@ -57,6 +72,9 @@ class Tractome(QtWidgets.QWidget):
             qt_parent=self,
             window_type="qt",
         )
+
+        self.show_manager.renderer.add_event_handler(self._on_key_press, "key_down")
+
         layout = QtWidgets.QHBoxLayout()
         control_container = QtWidgets.QWidget()
         control_container.setFixedWidth(200)
@@ -92,6 +110,11 @@ class Tractome(QtWidgets.QWidget):
             self._mesh_actor.local.position = (0, 0, 0)
             self.scene.add(self._mesh_actor)
 
+        if self._t1 is not None:
+            self._t1_actor = actor.slicer(self._t1)
+            self.scene.add(self._t1_actor)
+            self._t1_actor.local.position = (0, 0, 0)
+
     @property
     def streamlines(self):
         return self._streamlines
@@ -106,15 +129,18 @@ class Tractome(QtWidgets.QWidget):
         self.show_manager.render()
 
 
-def tractome(tractogram_path=None, mesh_path=None):
+def tractome(tractogram_path=None, mesh_path=None, t1_path=None):
     streamlines = None
     plydata = None
+    t1 = None
     if tractogram_path is not None:
         sft = load_tractogram(tractogram_path, "same", bbox_valid_check=False)
         streamlines = sft.streamlines
     if mesh_path is not None:
         plydata = PlyData.read(mesh_path)
-    tractome_app = Tractome(streamlines=streamlines, mesh=plydata)
+    if t1_path is not None:
+        t1, _ = load_nifti(t1_path)
+    tractome_app = Tractome(streamlines=streamlines, mesh=plydata, t1=t1)
     tractome_app.show_manager.start()
 
 
@@ -123,4 +149,7 @@ if __name__ == "__main__":
         "~/.dipy/bundle_atlas_hcp842/Atlas_80_Bundles/bundles/AC.trk"
     )
     mesh_path = os.path.expanduser("~/Downloads/sub-01_epo-01.ply")
-    tractome(tractogram_path, mesh_path=mesh_path)
+    t1_path = os.path.expanduser(
+        "~/.dipy/mni_template/mni_icbm152_t1_tal_nlin_asym_09a.nii"
+    )
+    tractome(tractogram_path, mesh_path=mesh_path, t1_path=t1_path)
